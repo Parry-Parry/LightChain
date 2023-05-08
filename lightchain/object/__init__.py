e5acd38b4365b7324799d75dd3c32eb54cdc960b
Andrew Parry <2393265p@student.gla.ac.uk>
2023-05-08 21:30:19 +0100

    Refactor

--- lightchain/object/__init__.py
+++ lightchain/object/__init__.py
@@ -2,62 +2,11 @@ from abc import ABC, abstractmethod
 import types
 from typing import Any
 from matchpy import Wildcard, Operation, Arity
-from chain import Chain, LambdaChain
-
-def get_chain(chain) -> Chain:
-
-    if isinstance(chain, Wildcard):
-        return chain
-    if isinstance(chain, Chain):
-        return chain
-    if isinstance(chain, list):
-        return SequentialPipeline(chain)
-    if isinstance(chain, types.FunctionType):
-        return LambdaChain(chain)
-    
-    raise ValueError("Passed parameter %s of type %s cannot be coerced into a chain" % (str(chain), type(chain)))
-
-class Pipeline(Operation):
-    name = 'Pipeline'
-    arity = Arity.polyadic
-
-    def __init__(self, operands, **kwargs):
-        super().__init__(operands=operands, **kwargs)
-        self.chains = list(map(lambda x : get_chain(x), operands) )
-
-    def __getitem__(self, i) -> Chain:
-        return self.chains[i]
-
-    def __len__(self) -> int:
-        return len(self.chains)
-
-    @abstractmethod
-    def __call__(self, input) -> Any:
-        raise NotImplementedError
-
-class SequentialPipeline(Pipeline):
-    name = 'Sequential Chain Pipeline'
-
-    def __init__(self, operands, **kwargs):
-        super().__init__(operands=operands, **kwargs)
-
-    def __call__(self, input) -> Any:
-        out = input
-        for chain in self.chains:
-            out = chain(out)
-        return out
-
-class ForkPipeline(Pipeline):
-    name = 'Forked Chain Pipeline'
-
-    def __init__(self, operands, **kwargs):
-        super().__init__(operands=operands, **kwargs)
-
-    def __call__(self, input) -> Any:
-        return {chain.name : chain(input) for chain in self.chains}
+from lightchain.pipeline import SequentialPipeline, ForkPipeline
 
 class Object(ABC):
     name = 'Object'
+
     def __init__(self, name='Entity', description='Some Standard Component') -> None:
         super().__init__()
         self.name = name
@@ -78,9 +27,11 @@ class Object(ABC):
     
 class Model(Object):
     name = 'Model'
+
     def __init__(self, model, name='Model', description='Some Standard Model') -> None:
         super().__init__(name=name, description=description)
         self.model = model
+
     def __call__(self, *args, **kwargs) -> Any:
         return self.model(*args, **kwargs)
 
