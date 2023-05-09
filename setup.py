64f490448c45f1a342943e5939d088b1c965ae7b
Andrew Parry <2393265p@student.gla.ac.uk>
2023-05-09 10:43:42 +0100

    Lightchain setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='lightchain',
+    version='0.0.1',
+    author='Andrew Parry',
+    author_email='a.parry.1@research.gla.ac.uk',
+    description="Stupidly Simple Chain Structures",
+    url='https://github.com/Parry-Parry/LightChain',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
