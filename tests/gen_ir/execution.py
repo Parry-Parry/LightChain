b0782a007010cc35b7125f36689b03f9d3bbffd1
Andrew Parry <2393265p@student.gla.ac.uk>
2023-06-15 00:24:18 +0100

    small changes

--- tests/gen_ir/execution.py
+++ tests/gen_ir/execution.py
@@ -1,5 +1,5 @@
 from lightchain.chain import Chain, LambdaChain, TerrierChain
-from lightchain.prompt import Prompt, FewShotPrompt
+from lightchain.prompt import Prompt
 from lightchain.object import Model
 import fire
 import ir_datasets as irds
@@ -12,8 +12,9 @@ def main(dataset, eval_set, model_name, cut=10):
     retriever = None % cut
     terrier = TerrierChain(model=retriever)
     parse = LambdaChain(lambda x: x)
+    collect = LambdaChain(lambda x : partial(parse_chains, chains=['keywords', 'entities', 'summary']))
 
-    keywords_prompt = Prompt('Generate Keywords that describe these documents \n {examples} Keyword:', ['examples'], name='keywords')
+    keywords_prompt = Prompt('Generate Keywords that describe these documents \n {examples} Keywords:', ['examples'], name='keywords')
     entities_prompt = Prompt('Generate a list of Entities that describe these examples \n {examples} \n Entities:', ['examples'], name='entities')
     summary_prompt = Prompt('Generate a Summary of these examples \n {examples} \n Summary:', ['examples'], name='summary')
 
@@ -21,8 +22,11 @@ def main(dataset, eval_set, model_name, cut=10):
 
     first_pass = terrier >> parse
     extract = keywords_prompt | entities_prompt | summary_prompt
+    expand = generate >> collect
 
-    chained_tasks = first_pass >> extract  >> generate
+    chained_tasks = first_pass >> extract  >> expand
+
+    expansions = chained_tasks(eval_set['query'].to_list())
 
 if __name__ == '__main__':
     fire.Fire(main)
