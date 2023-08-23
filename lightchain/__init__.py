76811fc1bf2e77c0db795523b5fc3717f8b7adcd
Andrew Parry <2393265p@student.gla.ac.uk>
2023-08-23 14:12:45 +0100

    legacy rubbisj

--- lightchain/__init__.py
+++ lightchain/__init__.py
@@ -1,5 +1,5 @@
 from .prompt.constructors import Prompt, FewShotPrompt
 from .chain import Chain, LambdaChain, SwitchBoardChain
-from .object import Object, Model
+from .object import Object
 
 __version__ = "0.0.2"
