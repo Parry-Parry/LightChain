9dbed099fdfce4f5e7af8c85fc9b6603f66aefa4
Andrew Parry <2393265p@student.gla.ac.uk>
2023-08-24 11:33:28 +0100

    needs to inherit object

--- lightchain/object/__init__.py
+++ lightchain/object/__init__.py
@@ -4,7 +4,46 @@ from abc import ABC, abstractmethod
 from typing import Any, Iterable
 from matchpy import Operation, Arity
 
-class Pipeline(Operation):
+class Object(ABC):
+    name = 'Object'
+
+    def __init__(self, name : str = 'Entity', description : str = 'Some Standard Entity') -> None:
+        super().__init__()
+        self.name = name
+        self.description = description
+
+    @abstractmethod
+    def __call__(self, *args, **kwargs) -> Any:
+        raise NotImplementedError
+    
+    def __rshift__(self, right):
+        return SequentialPipeline(self, right)
+    
+    def __lshift__(self, left):
+        return SequentialPipeline(left, self)
+    
+    def __or__(self, right):
+        return ForkPipeline(self, right)
+
+def get_chain(chain) -> Any:
+    from lightchain.prompt import Prompt
+    from lightchain.chain import Chain, LambdaChain
+    if isinstance(chain, Wildcard):
+        return chain
+    if isinstance(chain, Chain):
+        return chain
+    if isinstance(chain, Prompt):
+        return chain
+    if issubclass(type(chain), Object):
+        return chain
+    if isinstance(chain, list):
+        return SequentialPipeline(chain)
+    if isinstance(chain, types.FunctionType):
+        return LambdaChain(chain)
+    
+    raise ValueError("Passed parameter %s of type %s cannot be coerced into a chain" % (str(chain), type(chain)))
+
+class Pipeline(Object, Operation):
     name = 'Pipeline'
     arity = Arity.polyadic
 
@@ -46,42 +85,3 @@ class ForkPipeline(Pipeline):
         if isinstance(input, dict):
             return {k : self(v) for k, v in input.items()}
         return {chain.name : chain(input) for chain in self.chains}
-    
-class Object(ABC):
-    name = 'Object'
-
-    def __init__(self, name : str = 'Entity', description : str = 'Some Standard Entity') -> None:
-        super().__init__()
-        self.name = name
-        self.description = description
-
-    @abstractmethod
-    def __call__(self, *args, **kwargs) -> Any:
-        raise NotImplementedError
-    
-    def __rshift__(self, right):
-        return SequentialPipeline(self, right)
-    
-    def __lshift__(self, left):
-        return SequentialPipeline(left, self)
-    
-    def __or__(self, right):
-        return ForkPipeline(self, right)
-
-def get_chain(chain) -> Any:
-    from lightchain.prompt import Prompt
-    from lightchain.chain import Chain, LambdaChain
-    if isinstance(chain, Wildcard):
-        return chain
-    if isinstance(chain, Chain):
-        return chain
-    if isinstance(chain, Prompt):
-        return chain
-    if issubclass(type(chain), Object):
-        return chain
-    if isinstance(chain, list):
-        return SequentialPipeline(chain)
-    if isinstance(chain, types.FunctionType):
-        return LambdaChain(chain)
-    
-    raise ValueError("Passed parameter %s of type %s cannot be coerced into a chain" % (str(chain), type(chain)))
