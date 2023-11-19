7ae93ac86124f55ae1e923dbcc5c92cc3f137def
Andrew Parry <2393265p@student.gla.ac.uk>
2023-11-19 20:59:00 +0000

    beginning basic dsp

--- lightchain/__init__.py
+++ lightchain/__init__.py
@@ -1,5 +1,6 @@
 from .prompt import Prompt, FewShotPrompt
 from .chain import Chain, LambdaChain, SwitchBoardChain
 from .object import Object
+from .memory import Memory, QueueMemory, DictMemory, StringLengthBuffer, ConversationMemory
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
---
+++ lightchain/dsp/evaluate.py
@@ -0,0 +1,16 @@
+from lightchain import Object, Chain
+from typing import Any
+
+def Evaluator(Object):
+    def __init__(self,
+                 valset : Any,
+                 inner_eval : Any):
+        self.valset = valset
+        self._inner_eval = inner_eval
+    
+    def _run_val(self, chain : Chain, inner_kwargs : Any = None):
+        output = [chain(x) for x, _ in self.valset]
+        target = [y for _, y in self.valset]
+        return self._inner_eval(output, target, **inner_kwargs)
+
+    
